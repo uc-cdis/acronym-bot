90e945267c114efd3566723d4428ec5e5ddaf0e4
Marcelo Costa <marceloc@uchicago.edu>
2019-10-28 15:46:25 -0500

    fix Dockerfile, tweak setup and add requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM quay.io/cdis/py27base:pybase2-1.0.2
 ENV DEBIAN_FRONTEND=noninteractive
 
 # the www-data user is created in the parent image, just reusing it here
-RUN mkdir /opt/ctds/acronymbot \
+RUN mkdir -p /opt/ctds/acronymbot \
     && chown www-data /opt/ctds/acronymbot
 
 COPY . /acronymbot
--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
-#!/usr/bin/env python
-
-from setuptools import setup
-
-name = "acronym-bot"
-version = "0.1"
+from setuptools import setup, find_packages
 
 setup(
-    name=name,
-    version=version,
-    description="A Slack Bot to expand CTDS acronyms",
-    license="GPLv3",
+    name="acronymbot",
+    version="0.1",
+    description="Slack bot to expand CTDS acronyms",
     url="https://github.com/uc-cdis/acronym-bot",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GPLv3 License",
-    ],
-    py_modules=["acronymbot"],
-    install_requires=[
-        "requests==2.22.0",
-        "SlackClient==2.2.1"
-    ],
-    extras_require={},
-    entry_points={"console_scripts": ["acronymbot = acronymbot:main"]},
-    setup_requires=[],
+    license="Apache",
+    packages=find_packages(),
 )
